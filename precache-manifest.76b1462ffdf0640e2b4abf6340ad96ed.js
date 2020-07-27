eefec8deeb7b84fac74196ce429b656f3358f743
Franciszek Job <jellybean@onet,eu>
2020-07-27 14:59:18 +0200

    Updates

---
+++ precache-manifest.76b1462ffdf0640e2b4abf6340ad96ed.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2c6f61a2aefb2fb54994114e915eef1",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "6c6a8e4b403e19ff8d43",
+    "url": "/weather-app/static/css/main.cb1faf5b.chunk.css"
+  },
+  {
+    "revision": "d36834ddd497d19b90df",
+    "url": "/weather-app/static/js/2.2e93365d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weather-app/static/js/2.2e93365d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c6a8e4b403e19ff8d43",
+    "url": "/weather-app/static/js/main.44d049a6.chunk.js"
+  },
+  {
+    "revision": "d0e2e579b5155f255804",
+    "url": "/weather-app/static/js/runtime-main.e6f382d7.js"
+  }
+]);
